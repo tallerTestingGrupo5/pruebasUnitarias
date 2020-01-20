f6cc38464b6e9e63df21df38d550a4fd76da8196
CltControl <CltControl@GGIECUT1WRK043.espol.edu.ec>
2020-01-20 10:56:29 -0500

    commit

--- tallerTesting/src/tallerTesting/employeeTest.java
+++ tallerTesting/src/tallerTesting/employeeTest.java
@@ -1,5 +1,17 @@
 package tallerTesting;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class employeeTest {
 
+	 @Test
+	   public void salarioTipoWorker() {
+	      System.out.println("Siendo el empleado de tipo Worker ...");
+	      Employee E = new Employee((float) 386.0, "USD" ,0, EmployeeType.Worker);
+	      assertTrue(E.cs() == (float)402.083);
+	   }
+
+	
 }
